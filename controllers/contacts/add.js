063ae3831be5c313590a94e8298969a463e9bd25
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2023-12-12 21:16:11 +0200

    Update add.js

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,10 +1,11 @@
 const { Contact } = require('../../models/contact');
-const { ctrlWrapper } = require('../../utils');
+const { ctrlWrapper, uploadImage } = require('../../utils');
 
 const add = async (req, res, next) => {
+  const { url: avatar } = await uploadImage(req.file);
   const { _id: owner } = req.user;
 
-  const result = await Contact.create({ ...req.body, owner });
+  const result = await Contact.create({ ...req.body, avatar, owner });
   result.owner = undefined;
 
   res.status(201).json(result);
